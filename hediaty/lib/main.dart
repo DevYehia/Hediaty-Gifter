ca07c05ee7bfa3a06a4d32bf9eb30c61c68809b2
DevYehia <yehiayes20@gmail.com>
2024-10-30 22:30:25 +0300

    Started Making the SQLite DB

--- hediaty/lib/main.dart
+++ hediaty/lib/main.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:hediaty/Pages/mainPage.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+void initDB() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  databaseFactory = databaseFactoryFfi;
+  print(await getDatabasesPath());
+  final database = openDatabase(
+  // Set the path to the database. Note: Using the `join` function from the
+  // `path` package is best practice to ensure the path is correctly
+  // constructed for each platform.
+
+
+  join(await getDatabasesPath(), '.db'),
+  // When the database is first created, create a table to store dogs.
+  onCreate: (db, version) {
+    // Run the CREATE TABLE statement on the database.
+    return db.execute(
+      'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+    );
+  },
+  // Set the version. This executes the onCreate function and provides a
+  // path to perform database upgrades and downgrades.
+  version: 1,
+);
+}
+
+void main() async{
+  initDB();
   runApp(const MyApp());
 }
 
